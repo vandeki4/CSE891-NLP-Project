aaf012e08fc31c7ff71314026fffbb38ffb7f489
Zachary McCullough <mccul157@msu.edu>
2020-03-12 15:46:14 -0400

    adding scripts and unittests
    base made

---
+++ src/unittests/unittest.py
@@ -0,0 +1,11 @@
+import unittest
+from ..scripts.utility import *
+
+class MyTestCase(unittest.TestCase):
+    def pair_wise_1(self):
+        file1 = ''
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
